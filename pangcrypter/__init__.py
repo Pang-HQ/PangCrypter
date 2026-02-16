340450647e3c629469b9a4928e26e0a184200c5a
pang-dev <dev@panghq.com>
2026-02-16 00:24:20 +0000

    feat(version): bump version to 1.2.2

--- pangcrypter/__init__.py
+++ pangcrypter/__init__.py
@@ -2,6 +2,6 @@
 PangCrypter - A secure text editor with encryption capabilities.
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __author__ = "Pang HQ"
 __description__ = "Secure text editor."
