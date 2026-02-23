254b9063bde6a484c0ea290c921dd44cd37a17bf
pang-dev <dev@panghq.com>
2026-02-23 05:39:37 +0000

    chore(release): prepare v1.3.0

--- pangcrypter/__init__.py
+++ pangcrypter/__init__.py
@@ -2,6 +2,6 @@
 PangCrypter - A secure text editor with encryption capabilities.
 """
 
-__version__ = "1.2.3"
+__version__ = "1.3.0"
 __author__ = "Pang HQ"
 __description__ = "Secure text editor."
